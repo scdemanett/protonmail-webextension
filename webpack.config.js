a7260eae2e9d643c6d543f89a3925eb7ea07f276
Steven Demanett <stevendemanett@gmail.com>
2024-07-22 23:13:00 -0400

    Update Domain, Attempting To Update Dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,98 @@
-const path = require("path");
-const webpack = require("webpack");
-const fs = require("fs");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const fs = require('fs');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const env = {};
-const vars = fs.readFileSync("./.env").toString().split("\n");
+const vars = fs.readFileSync('./.env').toString().split('\n');
 for (const envVar of vars) {
-    let val = envVar.replace(/^[^=]*=/g, "");
-    if (val.indexOf("base64:") === 0) {
-        val = Buffer.from(val.replace(/base64:/, ""), "base64").toString();
+    let val = envVar.replace(/^[^=]*=/g, '');
+    if (val.indexOf('base64:') === 0) {
+        val = Buffer.from(val.replace(/base64:/, ''), 'base64').toString();
     }
-    env[envVar.replace(/=.*$/g, "")] = val;
+    env[envVar.replace(/=.*$/g, '')] = val;
 }
 
 module.exports = (webpackEnv) => {
     webpackEnv = webpackEnv || {};
 
     return {
-        entry: "./src/background/index.ts",
+        mode: webpackEnv.NODE_ENV === 'development' ? 'development' : 'production',
+        entry: {
+            background: './src/background/index.ts',
+            app: './src/index.tsx', // Main entry for the React app
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'build'),
+            clean: true,
+        },
         plugins: [
             new webpack.EnvironmentPlugin(env),
-            new CopyWebpackPlugin([{
-                from: "node_modules/webextension-polyfill/dist/browser-polyfill.js",
-            },
-            {
-                from: "src/_locales",
-                to: "_locales",
-            }]),
+            new CopyWebpackPlugin({
+                patterns: [
+                    {
+                        from: 'node_modules/webextension-polyfill/dist/browser-polyfill.js',
+                    },
+                    {
+                        from: 'src/_locales',
+                        to: '_locales',
+                    },
+                    {
+                        from: 'public',
+                        to: '', // Copy public assets to the build folder
+                        globOptions: {
+                            ignore: ['**/index.html'], // Avoid copying index.html
+                        },
+                    },
+                ],
+            }),
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+                chunks: ['app'],
+                inject: 'body',
+            }),
         ],
         module: {
             rules: [
                 {
-                    test: /\.ts$/,
+                    test: /\.tsx?$/,
                     exclude: /node_modules/,
-                    loader: require.resolve("ts-loader"),
-                    options: {
-                        context: __dirname,
-                        configFile: "tsconfig.background.json",
+                    use: 'ts-loader',
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: 'babel-loader',
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.s[ac]ss$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg)$/,
+                    type: 'asset/resource',
+                    generator: {
+                        filename: 'static/media/[name].[hash:8][ext]',
                     },
                 },
             ],
         },
         resolve: {
-            extensions: [".ts", ".js", ".json"],
+            extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
         },
-        output: {
-            filename: "background.js",
-            path: path.resolve(__dirname, "build"),
+        devtool: webpackEnv.NODE_ENV === 'development' ? 'source-map' : undefined,
+        devServer: {
+            historyApiFallback: true,
+            open: true,
+            hot: true,
+            port: 3000,
         },
-        target: "web",
-        devtool: webpackEnv.NODE_ENV === "development" ? "source-map" : undefined,
-        watch: webpackEnv.NODE_ENV === "development",
+        target: 'web',
     };
 };
